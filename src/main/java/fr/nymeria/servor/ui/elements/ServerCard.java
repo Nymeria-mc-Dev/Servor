bb15bf8cbc10be30bdc32026e94e9290e45c004a
julfit <julien.monteillet2007@gmail.com>
2022-08-13 00:49:08 +0200

    correction

--- src/main/java/fr/nymeria/servor/ui/elements/ServerCard.java
+++ src/main/java/fr/nymeria/servor/ui/elements/ServerCard.java
@@ -10,9 +10,9 @@ public class ServerCard {
 	public ServerCard(double x, double y) {
 		
 		if(y < 40) y = 40;
-		
+
 		Rectangle card = new Rectangle();
-		
+
 		card.setTranslateX(x);
 		card.setTranslateY(y);
 		card.setWidth(309);
@@ -20,23 +20,23 @@ public class ServerCard {
 		card.setArcHeight(15);
 		card.setArcWidth(15);
 		card.getStyleClass().add("serverCard");
-		
-		
+
+
 		Rectangle button = new Rectangle();
-		
+
 		button.setTranslateX(x);
 		button.setTranslateY(y + 131);
 		button.setWidth(309);
 		button.setHeight(39);
 		button.getStyleClass().add("gestionButton");
-		
+
 		Text gestion = new Text("Gestion");
-		gestion.setTranslateX(x);
+		gestion.setTranslateX(x + 110.25);
 		gestion.setTranslateY(y + 157);
 		gestion.setStyle("-fx-font-size: 24px; -fx-font-familly: regular;");
 		gestion.setFill(Color.WHITE);
-		
+
 		App.addItem(card, button, gestion);
 	}
-	
+
 }
