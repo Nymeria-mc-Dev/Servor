5ed546d427fb233c0aa9f3e710ecbf73ff485871
SorenDeveloppement <sorenel@yahoo.com>
2022-08-12 17:32:39 +0200

    finihing front of add server

--- src/main/java/fr/nymeria/servor/ui/AddServerButton.java
+++ src/main/java/fr/nymeria/servor/ui/AddServerButton.java
@@ -3,6 +3,8 @@ package fr.nymeria.servor.ui;
 import de.jensd.fx.glyphs.GlyphsDude;
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon;
 import fr.nymeria.servor.App;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
@@ -10,10 +12,18 @@ public class AddServerButton {
 
 	public AddServerButton(Stage stage) {
 		
-		Text button = GlyphsDude.createIcon(FontAwesomeIcon.PLUS);
+		Circle rect = new Circle(25);
+		rect.setFill(Color.BLUE);
+		rect.setTranslateX(1020);
+		rect.setTranslateY(650);
+		
+		Text button = GlyphsDude.createIcon(FontAwesomeIcon.PLUS, "50px");
+		button.setTranslateX(1026);
+		button.setTranslateY(653.25);
+		button.setFill(Color.WHITE);
 		
 		
-		App.addItem(button);
+		App.addItem(rect, button);
 	}
 
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module fr.nymeria.servor {
     requires javafx.controls;
     requires javafx.fxml;
 	requires fontawesomefx;
+	requires javafx.graphics;
 
     opens fr.nymeria.servor to javafx.fxml;
     exports fr.nymeria.servor;
