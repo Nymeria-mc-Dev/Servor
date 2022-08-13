30492191f52a8d22e580ee2bf5127cb6561bb48b
SorenDeveloppement <sorenel@yahoo.com>
2022-08-13 16:31:19 +0200

    animation of add server button

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module fr.nymeria.servor {
     requires javafx.fxml;
 	requires fontawesomefx;
 	requires javafx.graphics;
+	requires javafx.base;
 
     opens fr.nymeria.servor to javafx.fxml;
     exports fr.nymeria.servor;
