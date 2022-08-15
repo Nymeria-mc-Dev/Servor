ea3f83e6a1ca04260c74d776a35cba27d1c84792
julfit <julien.monteillet2007@gmail.com>
2022-08-15 22:47:50 +0200

    removing unused folder

--- src/main/java/fr/nymeria/servor/helpers/DiscordHelper.java
+++
@@ -1,9 +0,0 @@
-package fr.nymeria.servor.helpers;
-
-import club.minnced.discord.rpc.DiscordRPC;
-import club.minnced.discord.rpc.DiscordRichPresence;
-
-public class DiscordHelper {
-
-	
-}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,7 +4,6 @@ module fr.nymeria.servor {
 	requires fontawesomefx;
 	requires javafx.graphics;
 	requires javafx.base;
-	requires java.discord.rpc;
 
     opens fr.nymeria.servor to javafx.fxml;
     exports fr.nymeria.servor;
