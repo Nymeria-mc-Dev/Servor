5c7679aa96b2e2b1b6b39fb26ad1ba89aa6f9637
SorenDeveloppement <soren.evrard.lorrain@gmail.com>
2023-08-12 23:27:50 +0200

    saving

--- src/main/java/fr/nymeria/servor/ui/elements/ParameterContentPage.java
+++ src/main/java/fr/nymeria/servor/ui/elements/ParameterContentPage.java
@@ -1,8 +1,34 @@
 package fr.nymeria.servor.ui.elements;
 
+import fr.nymeria.servor.helpers.Settings;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+
+import javax.swing.*;
 
 public class ParameterContentPage {
     public ParameterContentPage(Pane pane) {
+
+        VBox root = new VBox();
+        root.setTranslateY(55.0d);
+        root.setTranslateX(355.0d);
+
+        TextField serverNameField = new TextField(Settings.ServerVersion);
+        serverNameField.setMinWidth(650.0d);
+        serverNameField.setMinHeight(70.0d);
+        serverNameField.setFont(Font.font("Poppins", FontWeight.NORMAL, 36));
+        serverNameField.setStyle("-fx-background-color: #363636; -fx-background-radius: 9px; -fx-alignment: center; -fx-text-fill: #fff");
+
+        root.getChildren().addAll(serverNameField);
+
+        pane.getChildren().add(root);
+
+    }
+
+    private TextField createTextField() {
+        return new TextField("Att julfit");
     }
 }
--- src/main/java/fr/nymeria/servor/ui/elements/SideParameterPanel.java
+++ src/main/java/fr/nymeria/servor/ui/elements/SideParameterPanel.java
@@ -16,7 +16,7 @@ public class SideParameterPanel {
         sidePanel.setStyle("-fx-background-color: #363636; ");
         sidePanel.setMinWidth(275.0d);
         sidePanel.setMinHeight(pane.getHeight());
-        sidePanel.setEffect(new DropShadow(15, Color.rgb(0, 0, 0, 0.8)));
+        sidePanel.setEffect(new DropShadow(25, Color.rgb(0, 0, 0, 0.8)));
 
         // Server version
         HBox serverVersionBox = new HBox();
