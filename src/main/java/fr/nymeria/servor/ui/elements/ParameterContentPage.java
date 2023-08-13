67ad6a9e066d23d16a02a6b2c105afdea3e4612e
SorenDeveloppement <soren.evrard.lorrain@gmail.com>
2023-08-13 11:02:54 +0200

    saving files

--- src/main/java/fr/nymeria/servor/ui/elements/ParameterContentPage.java
+++ src/main/java/fr/nymeria/servor/ui/elements/ParameterContentPage.java
@@ -1,6 +1,8 @@
 package fr.nymeria.servor.ui.elements;
 
 import fr.nymeria.servor.helpers.Settings;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
@@ -28,7 +30,7 @@ public class ParameterContentPage {
         serverPortBox.setTranslateY(50.0d);
 
         Text serverPort = new Text("Server Port : ");
-        serverPort.setTranslateY(10.0d);
+        serverPort.setTranslateY(4.0d);
         serverPort.setFill(Color.WHITE);
         serverPort.setFont(Font.font("Poppins", FontWeight.BOLD, 20));
 
@@ -49,10 +51,19 @@ public class ParameterContentPage {
         serverMaxRamSlider.setTranslateY(20.0d);
         serverMaxRamSlider.setMinWidth(410.0d);
         serverMaxRamSlider.setMin(1024);
-        serverMaxRamSlider.setMax(Runtime.getRuntime().maxMemory());
+        serverMaxRamSlider.setMax(10240);
         serverMaxRamSlider.getStyleClass().add("slider");
 
-        TextField serverMaxRamField = createTextField("0", 122.0d, 60.0d, 20);
+        TextField serverMaxRamField = createTextField("1024 Mb", 122.0d, 60.0d, 20);
+        serverMaxRamField.setTranslateX(20.0d);
+
+        serverMaxRamSlider.valueProperty().addListener((observable, oldValue, newValue) -> {
+            serverMaxRamField.setText((int) serverMaxRamSlider.getValue() + " Mb");
+
+            int percent = (int) (100 *  (serverMaxRamSlider.getValue() - serverMaxRamSlider.getMin()) / (serverMaxRamSlider.getMax() - serverMaxRamSlider.getMin()));
+            System.out.println(percent);
+            serverMaxRamSlider.setStyle("-fx-background-color: linear-gradient(to right, #0066FF " + percent + "%, #363636 " + percent + "%);");
+        });
 
         serverMaxRamBox.getChildren().addAll(serverMaxRam, serverMaxRamSlider, serverMaxRamField);
 
