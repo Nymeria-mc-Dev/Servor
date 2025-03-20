dc8a1e5d92527fe56d7f6cd4cd55f1c77bf4b095
julfit <julien.monteillet2007@gmail.com>
2025-03-20 22:48:37 +0100

    remove unused dependecies

--- src/main/java/fr/nymeria/servor/ui/elements/Manage/ConsolePane.java
+++ src/main/java/fr/nymeria/servor/ui/elements/Manage/ConsolePane.java
@@ -3,9 +3,7 @@ package fr.nymeria.servor.ui.elements.Manage;
 import fr.nymeria.servor.ui.elements.Manage.components.ConsoleComponent;
 import fr.nymeria.servor.ui.elements.Manage.components.ServerInteruptController;
 import fr.nymeria.servor.ui.elements.Manage.components.ServerUsageGraphComponent;
-import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
-import javafx.scene.control.ScrollBar;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
@@ -29,7 +27,7 @@ public class ConsolePane {
         HBox serverUsage = new HBox();
         serverUsage.setAlignment(Pos.CENTER);
 
-        ServerUsageGraphComponent serverCPUUsageGraphComponent = new ServerUsageGraphComponent(330.0d, 175.0d, 0.0d, 0d, "CPU Usage", 10, new float[]{20, 80, 100, 90, 40, 50, 30, 50, 50, 40});
+        ServerUsageGraphComponent serverCPUUsageGraphComponent = new ServerUsageGraphComponent(330.0d, 175.0d, 0.0d, 0d, "CPU Usage", 10, new float[]{20, 80, 100, 90, 100, 10, 1, 50, 50, 40});
         ServerUsageGraphComponent serverRAMUsageGraphComponent = new ServerUsageGraphComponent(330.0d, 175.0d, 100.0d, 0d, "RAM Usage", 10, new float[]{20, 40, 50, 80, 90, 60, 70, 50, 70, 60});
 
         serverUsage.getChildren().addAll(serverCPUUsageGraphComponent.get(), serverRAMUsageGraphComponent.get());
